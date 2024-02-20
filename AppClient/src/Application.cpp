691710cc9b819f26c7cf84c19017e6a4a95efb4b
VoidRune <mhbernik@gmail.com>
2024-02-20 18:05:10 +0100

    Quick test

--- AppClient/src/Application.cpp
+++ AppClient/src/Application.cpp
@@ -5,11 +5,18 @@
 
 Application::Application()
 {
+	std::cout << "Iput ip address and port as such:" << std::endl;
+	std::cout << "XXX.X.X.X:YYYY" << std::endl;
+	std::cout << "Where X is ip and Y is port like 127.0.0.1:60000" << std::endl;
+
+	std::string ip;
+	std::getline(std::cin, ip);
+
 	m_Client = std::make_unique<Arc::Client>();
 	m_Client->SetServerConnectedCallback([this]() { OnConnected(); });
 	m_Client->SetServerDisconnectedCallback([this]() { OnDisconnected(); });
 	m_Client->SetDataReceivedCallback([this](const Arc::Buffer data) { OnDataReceived(data); });
-	m_Client->ConnectToServer("127.0.0.1:60000");
+	m_Client->ConnectToServer(ip.c_str());
 }
 
 Application::~Application()
